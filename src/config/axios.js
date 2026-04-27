4c710a1bfe43e7a23a53282de01633b6a4d7bcf4
sunita <ssunitamkumbar@gmail.com>
2026-04-28 00:11:39 +0530

    updated files

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3054",
+  // baseURL: "http://localhost:3054",
+  baseURL:"https://inviprint-1.onrender.com",
+  
 });
 
 
